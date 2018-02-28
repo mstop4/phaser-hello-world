288a8282336740529816fdfaeb3984f553c1fe2c
mstop4 <mstop4@gmail.com>
2018-02-27 19:05:34 -0500

    platforms part 3

--- public/js/platform.js
+++ public/js/platform.js
@@ -2,6 +2,13 @@ var config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false
+    }
+  },
   scene: {
       preload: preload,
       create: create,
@@ -24,6 +31,14 @@ function preload () {
 function create () {
   this.add.image(400, 300, 'sky');
   this.add.image(400, 300, 'star');
+
+  platforms = this.physics.add.staticGroup();
+
+  platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+  platforms.create(600, 400, 'ground');
+  platforms.create(50, 250, 'ground');
+  platforms.create(750, 220, 'ground');
 }
 
 function update () {
